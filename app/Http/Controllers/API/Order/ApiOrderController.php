bb4bc81faad91350b41012c983944baefd92c7c0
Barta24.com <barta@barta24.local>
2021-01-09 13:18:46 +0600

    order apis updated

--- app/Http/Controllers/API/Order/ApiOrderController.php
+++ app/Http/Controllers/API/Order/ApiOrderController.php
@@ -6,7 +6,7 @@ use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 
 use App\Models\Order\Order;
-use App\Http\Resources\OrderResource;
+use App\Http\Resources\Order\OrderResource;
 
 use Illuminate\Support\Facades\Auth;
 
