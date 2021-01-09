207dde98ea2ae8683869689f58c37a030e71f95c
sanwarul <sanwarul.fuzzionit@gmail.com>
2021-01-09 16:11:40 +0600

    Working blade permissions

--- app/Http/Controllers/Backend/DashboardController.php
+++ app/Http/Controllers/Backend/DashboardController.php
@@ -12,7 +12,8 @@ class DashboardController extends Controller
     {
         $data['usersCount'] = User::count();
         $data['rolesCount'] = Role::count();
-        $data['users'] = User::orderBy('last_login_at','desc')->take(10)->get();
+        $data['users']      = User::orderBy('last_login_at','desc')->take(10)->get();
+        
         return view('backend.dashboard', $data);
     }
 }
