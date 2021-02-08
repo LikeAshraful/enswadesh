58dcef62ded4a1998a1c78e516778f56f78296b3
Md. Ashraful Islam <maipalash35@gmail.com>
2021-02-08 17:29:57 +0600

    product create, delete, show api created

--- app/Http/Resources/Product/ProductResource.php
+++ app/Http/Resources/Product/ProductResource.php
@@ -14,6 +14,24 @@ class ProductResource extends JsonResource
      */
     public function toArray($request)
     {
-        return parent::toArray($request);
+        return [
+            'ref'          => $this->ref,
+            'name'          => $this->name,
+            'slug'          => $this->slug,
+            'shop'        => $this->shop,
+            'user'       => $this->user,
+            'brand'    => $this->brand,
+            'thumbnail'          => $this->thumbnail,
+            'can_bargain'          => $this->can_bargain,
+            'product_type'          => $this->product_type,
+            'refund_policy'        => $this->refund_policy,
+            'service_policy'       => $this->service_policy,
+            'description'    => $this->description,
+            'thumbnail'          => $this->thumbnail,
+            'offers'          => $this->offers,
+            'tag'          => $this->tag,
+            'total_stocks'        => $this->total_stocks,
+            'created_at'    => $this->created_at->diffForHumans()
+        ];
     }
 }
