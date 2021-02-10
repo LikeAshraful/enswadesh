65d224db79a7a5ed4debb5b463110f965b5987e2
Md. Ashraful Islam <maipalash35@gmail.com>
2021-02-10 18:17:15 +0600

    product price added on api

--- app/Http/Resources/Product/ProductResource.php
+++ app/Http/Resources/Product/ProductResource.php
@@ -15,23 +15,25 @@ class ProductResource extends JsonResource
     public function toArray($request)
     {
         return [
-            'ref'          => $this->ref,
-            'name'          => $this->name,
-            'slug'          => $this->slug,
-            'shop'        => $this->shop,
-            'user'       => $this->user,
-            'brand'    => $this->brand,
-            'thumbnail'          => $this->thumbnail,
-            'can_bargain'          => $this->can_bargain,
-            'product_type'          => $this->product_type,
-            'refund_policy'        => $this->refund_policy,
-            'service_policy'       => $this->service_policy,
-            'description'    => $this->description,
-            'thumbnail'          => $this->thumbnail,
-            'offers'          => $this->offers,
-            'tag'          => $this->tag,
-            'total_stocks'        => $this->total_stocks,
-            'created_at'    => $this->created_at->diffForHumans()
+            'ref' => $this->ref,
+            'name' => $this->name,
+            'slug' => $this->slug,
+            'price' => $this->price,
+            'shop' => $this->shop,
+            'user' => $this->user,
+            'brand' => $this->brand,
+            'thumbnail' => $this->thumbnail,
+            'image' => $this->productImage,
+            'can_bargain' => $this->can_bargain,
+            'product_type' => $this->product_type,
+            'refund_policy' => $this->refund_policy,
+            'service_policy' => $this->service_policy,
+            'description' => $this->description,
+            'thumbnail' => $this->thumbnail,
+            'offers' => $this->offers,
+            'tag' => $this->tag,
+            'total_stocks' => $this->total_stocks,
+            'created_at' => $this->created_at->diffForHumans()
         ];
     }
 }
