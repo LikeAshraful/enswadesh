1daa57ba7b1c12b1f0bb537845e2175743e76238
sanwarul <sanwarul.fuzzionit@gmail.com>
2021-02-15 18:03:45 +0600

    Working on vendor staff api

--- app/Http/Controllers/API/UserManagement/VendorController.php
+++ app/Http/Controllers/API/UserManagement/VendorController.php
@@ -5,74 +5,57 @@ namespace App\Http\Controllers\API\UserManagement;
 use Illuminate\Http\Request;
 use App\Helpers\API\ApiHelpers;
 use Illuminate\Support\Facades\DB;
+use Repository\User\OtpRepository;
 use Repository\User\UserRepository;
 use App\Http\Controllers\Controller;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Database\QueryException;
 use App\Notifications\RegisteredUserMail;
+use App\Http\Controllers\JsonResponseTrait;
 use Illuminate\Support\Facades\Notification;
+use App\Http\Requests\API\User\SignUpRequest;
 use App\Http\Resources\Vendor\VendorResource;
-use App\Http\Requests\API\Staff\SignUpRequest;
 use App\Http\Requests\Users\UpdateUserRequest;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\JsonResponse;
 
 class VendorController extends Controller
 {
-    public $vendorRepo;
+    use JsonResponseTrait;
 
-    public function __construct(UserRepository $vendorRepository)
+    public $vendorRepo;
+    public $otpRepo;
+
+    public function __construct(UserRepository $vendorRepository, OtpRepository $otpRepository)
     {
         $this->vendorRepo = $vendorRepository;
+        $this->otpRepo  = $otpRepository;
     }
 
     public function index()
     {
-        try {
-            $users      = $this->vendorRepo->findByID(Auth::id());
-            $code       = Response::HTTP_FOUND;
-            $message    = $users->staffs->count() ." Staff Found!";
-            $response   = ApiHelpers::createAPIResponse(false, $code, $message, VendorResource::make($users), null);
-        } catch (QueryException $exception) {
-            $code       = $exception->getCode();
-            $message    = $exception->getMessage();
-            $response   = ApiHelpers::createAPIResponse(true, $code, $message, null, null);
-        }
-        return new JsonResponse($response, $code == Response::HTTP_FOUND ? Response::HTTP_FOUND : Response::HTTP_NO_CONTENT);
+        $users = $this->vendorRepo->findByID(Auth::id());
+        $staffs = $users->staffs->count() ." Staff Found!";
+        return $this->json('Staff list', [
+                'staffs'  => $staffs
+            ]);
     }
 
     public function store(SignUpRequest $request)
     {
-        $user = null;
-        DB::beginTransaction();
-        try {
-            $user = $this->vendorRepo->create($request->except('role_id','password') + [
-                'role_id'       =>5,
-                'password'      => Hash::make($request->password),
-            ]);
-            $this->vendorRepo->updateProfileByID($user->id,$request->except('user_id') + [
-                'user_id'       => $user->id
-            ]);
-            $user_verification= $this->vendorRepo->updateOtpByID($user->id,$request->except('user_id','otp','access_token') + [
-                'user_id'       => $user->id,
-                'otp'           => rand(1000, 9999),
-                'access_token'  => $user->createToken('authToken')->accessToken,
-            ]);
+        $user = DB::transaction(function () use ($request) {
+            $user = $this->vendorRepo->create($request->all());
+            $this->vendorRepo->updateOrNewBy($user);
+            $userOtp = $this->otpRepo->generateOtpForUser($user);
             $this->vendorRepo->staffVendorByID($user->id);
-            $token      = $user->createToken('authToken')->accessToken;
-            $code       = Response::HTTP_CREATED;
-            $message    = "user Successfully Registered.";
-            $response   = ApiHelpers::createAPIResponse(false, $code, $message, new VendorResource($user), $token);
-            Notification::send($user, new RegisteredUserMail());
-            DB::commit();
-        } catch (QueryException $exception) {
-            DB::rollback();
-            $message    = $exception->getMessage();
-            $code       = $exception->getCode();
-            $response   = ApiHelpers::createAPIResponse(true, $code, $message, null, null);
-        }
-        return new JsonResponse($response, $user ? Response::HTTP_CREATED : Response::HTTP_INTERNAL_SERVER_ERROR);
+            return compact('user', 'userOtp');
+        });
+
+        return $this->json('User registered successfully. Please check your email or phone to active account', [
+            'token' => $user['userOtp']['token'],
+            'otp'   => $user['userOtp']['otp']
+        ]);
     }
 
     public function show($id)
